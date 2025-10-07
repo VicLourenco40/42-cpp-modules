8478b38f3a15998f4163cc1df8cba369fcaf6e89
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-10-07 11:41:44 +0100

    Complete module 00 exercise 00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+
+void	capitalize_string(char* string);
+
+int	main(int argc, char** argv)
+{
+	if (argc < 2) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	argv++;
+	while (*argv) {
+		capitalize_string(*argv);
+		std::cout << *argv;
+		argv++;
+	}
+	std::cout << '\n';
+}
+
+void	capitalize_string(char* string)
+{
+	while (*string) {
+		*string = toupper(*string);
+		string++;
+	}
+}
