88723cc8c9aa509b4e6b4976fec6af94aba072bc
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-10-14 19:15:46 +0100

    Initialize HumanB weapon to NULL

--- module_01/ex03/HumanB.cpp
+++ module_01/ex03/HumanB.cpp
@@ -2,7 +2,7 @@
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(const std::string& name) : name(name) {}
+HumanB::HumanB(const std::string& name) : name(name), weapon(NULL) {}
 
 void HumanB::setWeapon(Weapon& newWeapon)
 {
