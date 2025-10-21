905f3e6fdccb10ed558b57380b38c26432fddf44
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-10-21 15:09:16 +0100

    Add virtual specifier to attack()

--- module_03/ex02/ClapTrap.hpp
+++ module_03/ex02/ClapTrap.hpp
@@ -20,7 +20,7 @@ public:
 	ClapTrap(const ClapTrap& other);
 	~ClapTrap();
 	ClapTrap& operator=(const ClapTrap& other);
-	void attack(const std::string& target);
+	virtual void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
 };
