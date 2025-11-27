17e8352cacec31bad13f4d7451ea307e29765882
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-11-27 15:49:04 +0000

    Complete module 05 exercise 00

---
+++ module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat
+{
+private:
+	static const int lowestGrade = 150;
+	static const int highestGrade = 1;
+
+	const std::string name;
+	int grade;
+
+	void setGrade(int grade);
+
+public:
+	Bureaucrat(void);
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat(const std::string& name, int grade);
+	~Bureaucrat(void);
+
+	Bureaucrat& operator=(const Bureaucrat& other);
+
+	const std::string& getName(void) const;
+	int getGrade(void) const;
+	void incrementGrade(void);
+	void decrementGrade(void);
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		const char* what(void) const throw();
+	};
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		const char* what(void) const throw();
+	};
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj);
+
+#endif
---
+++ module_05/ex00/main.cpp
@@ -0,0 +1,64 @@
+#include "Bureaucrat.hpp"
+
+#include <iostream>
+
+int	main(void)
+{
+	Bureaucrat a("a", 150);
+	std::cout << a << '\n';
+
+	Bureaucrat b("b", 1);
+	std::cout << b << '\n';
+
+	try
+	{
+		Bureaucrat c("c", 151);
+		std::cout << c << '\n';
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "c constructor failed: " << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat d("d", 0);
+		std::cout << d << '\n';
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "d constructor failed: " << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat e("e", 149);
+		std::cout << e << '\n';
+
+		e.decrementGrade();
+		std::cout << e << '\n';
+
+		e.decrementGrade();
+		std::cout << e << '\n';
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "e grade decrement failed: " << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat f("f", 2);
+		std::cout << f << '\n';
+
+		f.incrementGrade();
+		std::cout << f << '\n';
+
+		f.incrementGrade();
+		std::cout << f << '\n';
+	}
+	catch (const std::exception& f)
+	{
+		std::cout << "f grade increment failed: " << f.what() << '\n';
+	}
+}
